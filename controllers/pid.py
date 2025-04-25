70467c8d98262cfcd953af43670d1f37e88af860
adornelas <andredornelas23@gmail.com>
2025-04-25 18:46:56 -0300

    Remove commented-out PIDController code

--- controllers/pid.py
+++ controllers/pid.py
@@ -73,20 +73,3 @@ class FirmwareController:
 
         return u
     
-
-    # class PIDController:
-    # def __init__(self, kp, ki, kd, imax=5.0):
-    #     self.kp = kp
-    #     self.ki = ki
-    #     self.kd = kd
-    #     self.imax = imax
-    #     self.integral = 0
-    #     self.last_error = 0
-
-    # def update(self, error, dt):
-    #     self.integral += error * dt
-    #     self.integral = max(min(self.integral, self.imax), -self.imax)  # anti-windup
-    #     derivative = (error - self.last_error) / dt
-    #     self.last_error = error
-    #     u = self.kp * error + self.ki * self.integral + self.kd * derivative
-    #     return max(min(u, 1.0), -1.0)
