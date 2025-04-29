2f403005b762073e0e7227fac42b597fed239128
adornelas <andredornelas23@gmail.com>
2025-04-29 15:33:04 -0300

    Add WheelPIDController class for wheel speed control with PID logic

--- controllers/pid.py
+++ controllers/pid.py
@@ -73,3 +73,26 @@ class FirmwareController:
 
         return u
     
+class WheelPIDController:
+    def __init__(self, kp, ki, kd, imax=1.0):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.imax = imax
+        
+        self.integral = 0
+        self.last_error = 0
+
+    def update(self, omega_ref, omega_measured, dt):
+        error = omega_ref - omega_measured
+
+        self.integral += error * dt
+        self.integral = np.clip(self.integral, -self.imax, self.imax)
+
+        derivative = (error - self.last_error) / dt
+        self.last_error = error
+
+        pwm_output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        pwm_output = np.clip(pwm_output, -1.0, 1.0)  # Normalized PWM
+
+        return pwm_output
